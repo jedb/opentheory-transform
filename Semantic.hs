451dbbd9f4cc3c61f03e848811e3162f3f080c7f
Jed Barber <jjbarber@y7mail.com>
2012-06-07 09:10:17 +1000

    Inconsequential change

--- Semantic.hs
+++ Semantic.hs
@@ -347,7 +347,7 @@ doSemanticCheck list =
 
 
 main = do
-      args <- getArgs
-      list <- getLines $ head args
-      let result = doSemanticCheck (map (stripReturn) list)
-      printf result
+    args <- getArgs
+    list <- getLines $ head args
+    let result = doSemanticCheck (map (stripReturn) list)
+    printf result

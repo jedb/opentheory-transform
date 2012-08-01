5eeb784929c9bb3bc146aa2b5c2b1aab2e86a5ff
Jed Barber <jjbarber@y7mail.com>
2012-08-02 08:31:37 +1000

    Allows concatenation of article files

---
+++ Concat.hs
@@ -0,0 +1,16 @@
+import System( getArgs )
+import Text.Printf
+import Parse
+import ProofGraph
+import WriteProof
+
+
+
+main = do
+    args <- getArgs
+    x <- getLines $ args!!0
+    y <- getLines $ args!!1
+    let list = x ++ y
+        graph = doGraphGen (map (stripReturn) list)
+        trace = doWriteProof graph
+    output trace

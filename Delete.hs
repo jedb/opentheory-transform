ad0d628b1bf333a99238bd2cefc60ec059df3f81
Jed Barber <jjbarber@y7mail.com>
2012-08-02 08:32:06 +1000

    Removes theorems from article, specified by number

---
+++ Delete.hs
@@ -0,0 +1,20 @@
+import System( getArgs )
+import Text.Printf
+import Parse
+import ProofGraph
+import WriteProof
+
+import qualified Data.Graph.Inductive.Graph as Graph
+
+
+
+main = do
+	args <- getArgs
+	list <- getLines $ head args
+	let graph = doGraphGen (map (stripReturn) list)
+	    initList = filter (\x -> Graph.indeg graph x == 0) (Graph.nodes graph)
+	    thmList = (map (\(_,y) -> y)) .
+	              (filter (\(x,y) -> (show x) `notElem` (tail args))) .
+	              (zip [1..]) $ initList
+	    trace = writeAll graph thmList
+	output trace
